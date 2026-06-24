f5ddc35eefc1d2fafa951ebf7c89b9a813a2a45e
mdsak <mdsakilkhan649@gmail.com>
2026-06-25 02:38:51 +0600

    first commit

---
+++ class_object.dart
@@ -0,0 +1,12 @@
+class Student{
+    String name = "Sakil";
+    int roll =683100;
+
+    void display(){
+        print("My name is $name and my roll is $roll");
+    }
+}
+void main(){
+    Student s1 = Student();
+    s1.display();
+}
