1236551fba880516497f2f07d8f65207610b12e9
mdsak <mdsakilkhan649@gmail.com>
2026-06-25 22:48:40 +0600

    update

--- class_object.dart
+++ class_object.dart
@@ -1,12 +1,28 @@
+
+
+
+// ১. ক্লাস ডিক্লেয়ার করা (একদম C++ এর মতো)
 class Student{
+    // ডার্টে String এর 'S' সবসময় বড় হাতের (Capital) হয়!
     String name = "Sakil";
     int roll =683100;
-
+// ২. মেথড বা ফাংশন
     void display(){
+        // ম্যাজিকটা দেখুন! ভেরিয়েবল প্রিন্ট করতে শুধু $ চিহ্ন ব্যবহার করেছি।
         print("My name is $name and my roll is $roll");
     }
 }
+// ৩. মেইন ফাংশন (এখান থেকেই অ্যাপ রান হওয়া শুরু করে)
 void main(){
-    Student s1 = Student();
+    // ৪. অবজেক্ট তৈরি করা
+    Student s1 = Student();// C++ এর মতোই, শুধু ব্র্যাকেট () দিতে হয়।
+   // ৫. ফাংশন কল করা
     s1.display();
 }
+
+/*
+void main(){
+    int? age;
+    print("My age is $age");
+}
+*/
---
+++ constructor.dart
@@ -0,0 +1,51 @@
+
+
+/*
+class Student{
+  String name;
+  int roll;
+  Student(this.name, this.roll);
+  void display(){
+  print("Name: $name,Roll: $roll");
+  }
+}
+void main(){
+  Student s1 = Student("Sakil", 683100);
+  s1.display();
+}
+*/
+/*
+class Student{
+  String name;
+  int roll;
+  // কনস্ট্রাক্টরের প্যারামিটারগুলো {} এর ভেতর রাখা হয়েছে
+  // 'required' শব্দটার মানে হলো: এই ডাটাগুলো দিতেই হবে, না দিলে এরর দেবে!
+  Student({required this.name,required this.roll});
+  void display(){
+    print("Name: $name,Roll:$roll");
+  }
+
+}
+void main(){
+  // এখন অবজেক্ট বানানোর সময় নাম বলে বলে ডাটা দেওয়া যাচ্ছে!
+  // এতে কোন ডাটাটা কীসের, তা বুঝতে খুব সুবিধা হয়।
+  Student s1 = Student(name: "Sakil",roll: 683100);
+  // আপনি চাইলে আগে পিছে করেও ডাটা দিতে পারেন
+  Student s2 = Student(roll: 683101,name: "Sakib");
+  s1.display(); 
+  s2.display();
+}
+*/
+class Smartphone{
+  String brand;
+  int price;
+  Smartphone({required this.brand,required this.price});
+  void display(){
+    print("Brand: $brand,Price: $price");
+  }
+}
+void main(){
+  Smartphone s1 = Smartphone(brand: "Samsung",price: 30000);
+  s1.display();
+  
+}
